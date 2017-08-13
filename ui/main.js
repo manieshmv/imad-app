42d3d9382c3f92915644b7daa00dc735324bb372
manieshmv <30755285+manieshmv@users.noreply.github.com>
2017-08-13 17:51:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,11 @@ console.log('Loaded!');
 //moving the image on click
 var img = document.getElementById('mani');
 var marginLeft =0;
-function moveright() {
-    marginLeft = marginLeft=10;
+function moverRight() {
+    marginLeft = marginLeft + 10;
     img.styl.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
-    var interval = setInterval(moveright, 100);
+    var interval = setInterval(moveRight, 100);
     
 };
