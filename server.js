0cdb9103d17fb8c31ab6e86d60fb792cd4f5e27a
manieshmv <30755285+manieshmv@users.noreply.github.com>
2017-08-13 16:38:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', '31f845cdeaea9c9b6243b618118c181c.jpg'));
 });
 
 
